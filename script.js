75f7ae2e340bd341384c73666b1763db2dec732b
saradindurana <saradindurana@gmail.com>
2022-08-05 09:42:51 +0530

    chng

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["cse student", "programmer", "web developer"],
+        strings: ["CS Enthusiast", "programmer", "web developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["cse student", "programmer", "web developer"],
+        strings: ["CS Enthusiast", "programmer", "web developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
